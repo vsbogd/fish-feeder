a2c1b71d204df1e99d5b33f722bb016d8e0bf80f
Vitaly Bogdanov <vsbogd@gmail.com>
2025-11-30 10:35:58 +0300

    Add more constants and tuning instructions

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
 # Overview
 
 Fish feeder firmware for the https://www.thingiverse.com/thing:6378659 based on
-cheap Arduino Mini Pro Atmega168 8MHz.
+cheap Arduino Mini Pro Atmega168 8MHz. Link to my make:
+https://www.thingiverse.com/make:1242906
 
 It controls PWM pin 11 manually using Atmega168 registers. Thus changing pin in
 this code is not trivial. Constant in the code is provided for reference only.
 
-Please see my make for more info: https://www.thingiverse.com/make:1242906
+Other constants in the code allows you tuning it for the specific case:
+- `FIRST_FEED_SECONDS` - delay of the first feed after powering on in seconds
+  (default 30 seconds)
+- `FEEDING_PERIOD_HOURS` - feeding period in hours (default once in 24 hours)
+- `NUMBER_OF_PORTIONS` - number of portions to drop when feeding (default 1
+  portion)
+
+# Tuning
+
+After flashing the microcontroller, turn on the empty feeder and wait for 30
+seconds. You should see feeding disk going to the default position immediately
+after turning on. After `FIRST_FEED_SECONDS` delay disk should go into the
+feeding position and after a second go back. 
+
+If the angle between feeding and default position is 180 degrees but positions
+are not correct then unscrew the disk, turn it to the default position, screw
+it again and repeat the test.
+
+If the angle is not 180 degrees then set default position first as described
+above, then try to change `MIN_SIGNAL` and `MAX_SIGNAL` in order to change the
+angle. Default values should be ok for the SG90 servo drive but if you have
+another servo the additional tuning may be required.
+
+
--- fish_feeder.ino
+++ fish_feeder.ino
@@ -1,22 +1,28 @@
-#include <Servo.h>
-
 #define PIN_SERVO 11 // pin is driven via register and changing this define
-                     // doesn't automatically change the pin
+                     // doesn't automatically change the pin, this constant is
+                     // only for reference
 
-#define MIN_ANGLE 0
-#define MAX_ANGLE 180
-#define MIN_SIGNAL 31
-#define MAX_SIGNAL 160
+#define FIRST_FEED_SECONDS 30   // delay of the first feed after powering on in seconds
+#define FEEDING_PERIOD_HOURS 24 // feeding period, for example once in 24
+                                // hours
+#define NUMBER_OF_PORTIONS 1    // number of portions to drop when feeding
 
-#define OPENED 0
-#define CLOSED 180
+#define OPENED 0    // feeding position in degrees
+#define CLOSED 180  // position between feedings in degrees
+
+// Values below should be ok for SG90 servo which is used in original design
+// One may need to change them for another model or specific instance of the drive 
+#define MIN_ANGLE 0     // minimal angle for the servo
+#define MAX_ANGLE 180   // maximum angle for the servo
+#define MIN_SIGNAL 31   // the value to send on pin to get the minimal angle
+#define MAX_SIGNAL 160  // the value to send on pin to get the maximum angle
 
 unsigned long next_feed_ms = 0;
 
 void setup() {
   init_pwm();
   set_position(CLOSED);
-  next_feed_ms = millis() + 30 * 1000; // 30 seconds
+  next_feed_ms = millis() + FIRST_FEED_SECONDS * 1000;
 }
 
 void init_pwm() {
@@ -48,8 +54,8 @@ void to_position(float angle) {
  
 void loop() {
   if (millis() > next_feed_ms) {
-    next_feed_ms += 24 * 3600000; // 24 hours
-    for (int i = 0; i < 3; i++) {
+    next_feed_ms += FEEDING_PERIOD_HOURS * 3600000;
+    for (int i = 0; i < NUMBER_OF_PORTIONS; i++) {
       to_position(OPENED);
       delay(1000);
       to_position(CLOSED);
